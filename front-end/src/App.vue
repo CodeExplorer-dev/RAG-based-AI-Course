e9c722818f61db25e243782bc3fd26182a5a687f
myw <1686196831@qq.com>
2026-06-16 08:46:16 +0800

    fuck

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,8 +1,8 @@
 <!--
  * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
  * @Date: 2026-06-16 08:31:01
- * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
- * @LastEditTime: 2026-06-16 08:32:05
+ * @LastEditors: myw 1686196831@qq.com
+ * @LastEditTime: 2026-06-16 08:45:12
  * @FilePath: \RAG-based-AI-Course\front-end\src\App.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -12,5 +12,4 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <HelloWorld />
-  <h1>hhh</h1>
 </template>
